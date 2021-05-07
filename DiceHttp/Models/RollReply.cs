0bb6c00ff7a64266e13553e590e4c8fc493eea1b
jim bergstrom <jim@bergles.xyz>
2021-05-07 07:54:33 -0600

    Some final tweaks to DiceService

--- DiceHttp/Models/RollReply.cs
+++ DiceHttp/Models/RollReply.cs
@@ -1,7 +1,10 @@
+using System.Linq;
 namespace DiceHttp.Models
 {
     public class RollReply
     {
         public int[] Rolls { get; set; }
+        public int Sum => Rolls.Sum();
+        public double Mean => Rolls.Average();
     }
 }
